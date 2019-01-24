f9619633c6407d039f2ed6fc5ee1fc8d550adc10
Shehan <shehanpa@wso2.com>
2019-01-24 17:05:36 +0530

    Add changes requested

--- component/src/main/java/org/wso2/extension/siddhi/io/cdc/util/MyYamlConstructor.java
+++ component/src/main/java/org/wso2/extension/siddhi/io/cdc/util/MyYamlConstructor.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.extension.siddhi.io.cdc.util;
 
 import org.wso2.extension.siddhi.io.cdc.source.config.Database;
@@ -6,15 +24,16 @@ import org.yaml.snakeyaml.constructor.Constructor;
 import org.yaml.snakeyaml.error.YAMLException;
 import org.yaml.snakeyaml.nodes.Node;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class is used to read query-config.yaml
  */
 public class MyYamlConstructor extends Constructor {
 
-    private HashMap<String, Class<?>> classMap = new HashMap<String, Class<?>>();
+    private Map<String, Class<?>> classMap = new HashMap<>();
 
-    public MyYamlConstructor(Class<? extends Object> theRoot) {
+    public MyYamlConstructor(Class<?> theRoot) {
         super(theRoot);
         classMap.put(QueryConfiguration.class.getName(), QueryConfiguration.class);
         classMap.put(Database.class.getName(), Database.class);
@@ -22,11 +41,11 @@ public class MyYamlConstructor extends Constructor {
 
     protected Class<?> getClassForNode(Node node) {
         String name = node.getTag().getClassName();
-        Class<?> cl = classMap.get(name);
-        if (cl == null) {
+        Class<?> classForNode = classMap.get(name);
+        if (classForNode == null) {
             throw new YAMLException("Class not found: " + name);
         } else {
-            return cl;
+            return classForNode;
         }
     }
 
