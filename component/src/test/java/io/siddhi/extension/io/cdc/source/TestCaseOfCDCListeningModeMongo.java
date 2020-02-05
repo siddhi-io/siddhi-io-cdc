f037b3fed1eaa6872450b6665fd44b992d028a72
Prabod Dunuwila <praboddunuwila@gmail.com>
2020-02-05 20:05:12 +0530

    Add license headers & assertions for tests

--- component/src/test/java/io/siddhi/extension/io/cdc/source/TestCaseOfCDCListeningModeMongo.java
+++ component/src/test/java/io/siddhi/extension/io/cdc/source/TestCaseOfCDCListeningModeMongo.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.siddhi.extension.io.cdc.source;
 
 import io.siddhi.core.SiddhiAppRuntime;
@@ -64,7 +82,7 @@ public class TestCaseOfCDCListeningModeMongo {
                 " table.name = '" + collectionName + "', " +
                 " operation = 'insert', " +
                 " @map(type='keyvalue'))" +
-                "define stream istm (id string, name string, amount double, volume int);";
+                "define stream istm (name string, amount double, volume int);";
 
         String mongoStoreDefinition = "define stream insertionStream (name string, amount double, volume int);" +
                 "@Store(type='mongodb', mongodb.uri='" + databaseUri + "')" +
@@ -114,6 +132,11 @@ public class TestCaseOfCDCListeningModeMongo {
 
         //Assert event arrival.
         Assert.assertTrue(eventArrived.get());
+
+        Thread.sleep(1000);
+
+        //Assert event data.
+        Assert.assertEquals(insertingObject, currentEvent.getData());
 
         cdcAppRuntime.shutdown();
         mongoAppRuntime.shutdown();
