b647efb3015cec6c26d28df0f81ad8d5ca45ec23
Ajanthy <ajanthyjaya@gmail.com>
2020-05-19 16:12:19 +0530

    Change Exception

--- component/src/main/java/io/siddhi/extension/io/cdc/source/polling/CDCCronExecutor.java
+++ component/src/main/java/io/siddhi/extension/io/cdc/source/polling/CDCCronExecutor.java
@@ -77,7 +77,7 @@ public class CDCCronExecutor implements Job {
             // Tell quartz to schedule the job using our trigger
             scheduler.scheduleJob(cron, trigger);
         } catch (SchedulerException e) {
-            log.error("An error occurred when scheduling job in siddhi app for cdc polling " + e);
+            log.error("An error occurred when scheduling job in siddhi app for cdc polling " + e, e);
         }
     }
 
